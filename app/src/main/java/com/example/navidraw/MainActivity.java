b2b9e3a68739c614e144d26132e379b728b0bc07
dudi111 <pd.rajiv.000@gmail.com>
2021-07-22 21:13:07 +0530

    navi

---
+++ app/src/main/java/com/example/navidraw/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.navidraw;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MaterialToolbar materialToolbar=findViewById(R.id.material);
+        DrawerLayout drawerLayout=findViewById(R.id.draw);
+        NavigationView navigationView=findViewById(R.id.navi);
+        materialToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+}
